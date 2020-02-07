6dd4e68cca5262fca982ef795504893064a90c16
Jade <wangyujue19960222@gmail.com>
2020-02-06 19:44:04 -0500

    Init frontend

---
+++ frontend/sample/src/main.ts
@@ -0,0 +1,29 @@
+// import Vue from 'vue';
+// import App from './App.vue';
+// import router from './router';
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   router,
+//   render: (h) => h(App),
+// }).$mount('#app');
+
+import router from './router/index';
+
+import Vue from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
