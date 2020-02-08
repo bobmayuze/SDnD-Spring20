e0368de150f6599ba40e3fccb5bbb7b5e19eb521
Ming <lumingabby@outlook.com>
2020-02-08 13:56:20 -0500

    add routes and page index

--- frontend/sample/src/App.vue
+++ frontend/sample/src/App.vue
@@ -1,38 +1,55 @@
 <template>
-  <a-layout id="components-layout-demo-custom-trigger">
-    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
-      <div class="logo" />
-      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
-        <a-menu-item key="1">
-          <a-icon type="user" />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="()=> collapsed = !collapsed"
-        />
-      </a-layout-header>
-      <a-layout-content
-        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
-      >
-        Content
-      </a-layout-content>
+  <div id="app">
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+        <div class="logo">
+          <h1>CMS</h1>
+        </div>
+
+        <a-menu theme="dark" mode="inline">
+          <a-menu-item key="1">
+            <a-icon type="dashboard" />
+            <span>Home</span>
+            <a href="#/"></a>
+          </a-menu-item>
+
+          <a-menu-item key="2">
+            <a-icon type="rocket" />
+            <span>Templates</span>
+            <a href="#/templates"></a>
+          </a-menu-item>
+
+          <a-menu-item key="3">
+            <a-icon type="schedule" />
+            <span>Deployment Jobs</span>
+            <a href="#/jobs"></a>
+          </a-menu-item>
+
+          <!-- <a-menu-item key="4">
+            <a-icon type="global" />
+            <span>Regions</span>
+            <a href="#/regions"></a>
+          </a-menu-item> -->
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="()=> collapsed = !collapsed"
+          />
+        </a-layout-header>
+        <a-layout-content
+          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+        >
+          <router-view />
+        </a-layout-content>
+      </a-layout>
     </a-layout>
-  </a-layout>
+  </div>
 </template>
+
 <script>
   export default {
     data() {
