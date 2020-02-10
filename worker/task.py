0c7e54da8cb0504a63c6f14531d0e91f554c7ab2
Aneesh Venuturumilli <veeaneesh@gmail.com>
2020-02-10 17:58:59 -0500

    add starter code for region worker, create setup files

---
+++ worker/task.py
@@ -0,0 +1,13 @@
+from celery import Celery, Task 
+from pymongo import MongoClient
+import time
+
+import config
+
+app = Celery('tasks')
+app.config_from_object(config)
+
+@app.task()
+def add(x, y):
+    time.sleep(5)
+    return x + y
