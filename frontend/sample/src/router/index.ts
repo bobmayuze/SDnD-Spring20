5c70a12d2f27d4144f8ebf38431278357fde92fc
Jade <wangyujue19960222@gmail.com>
2020-02-10 18:46:19 -0500

    add template router

--- frontend/sample/src/router/index.ts
+++ frontend/sample/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
+import Templates from '../views/Templates.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'home',
     component: Home,
   },
+  {
+    path: '/templates',
+    name: 'template',
+    component: Templates,
+  }
 ];
 
 const router = new Router({
