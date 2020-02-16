747223d1c4ab4fd85e5dfd5a2b94e69b657a9940
Jade <wangyujue19960222@gmail.com>
2020-02-16 13:00:41 -0500

    add router

--- frontend/sample/src/router/index.ts
+++ frontend/sample/src/router/index.ts
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
 import Templates from '../views/Templates.vue';
+import Templates_create from '../views/Templates_create.vue';
+import Template_detailed_info from './views/Template_detailed_info.vue';
+import Template_update from './views/Template_update.vue';
+
 
 Vue.use(Router);
 
@@ -15,7 +19,26 @@ const routes = [
     path: '/templates',
     name: 'template',
     component: Templates,
+  },
+  {
+    path: '/templates/templates_create',
+    name: 'templates_create',
+    component: Templates_create,
+  },
+  {
+    path: '/templates/template_detailed_info',
+    name: 'template_detailed_info',
+    component: Template_detailed_info,
+  },
+  {
+    path: '/templates/template_update',
+    name: 'Template_update',
+    component: Template_update,
+  },
+  {
+    
   }
+
 ];
 
 const router = new Router({
