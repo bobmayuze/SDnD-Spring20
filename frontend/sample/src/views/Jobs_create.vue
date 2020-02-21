02bc133787947fdc4c5ac88896dd49f97b603318
Ming <lumingabby@outlook.com>
2020-02-20 19:43:45 -0500

    update jobs_create page and router

---
+++ frontend/sample/src/views/Jobs_create.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <a-row>
+      <a-col :span="6"></a-col>
+      <h1>New Deployment Jobs</h1>
+    </a-row>    
+    <a-form>
+      <a-row>
+        <a-col :span="6"></a-col>
+        <h2>Step 1 : Choose Templates to be deployed</h2>
+      </a-row>      
+      <a-form-item
+        v-bind="formItemLayout"
+        label="Templates"
+      >
+      </a-form-item>
+
+
+      <a-row>
+        <a-col :span="6"></a-col>
+        <h2>Step 2 : Choose Regions</h2>
+      </a-row>
+      <a-form-item
+        v-bind="formItemLayout"
+        label="China-North"
+      >
+      </a-form-item>
+
+      <!-- Submit Button -->
+      <a-row>
+        <a-col :span="6"></a-col>
+      </a-row>      
+    </a-form>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  data() {
+    this.formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 10 },
+    }
+    return {
+      data: [],
+      value: [],
+      fetching: false,
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
