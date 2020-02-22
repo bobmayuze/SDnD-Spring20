932af70cb1007294d3a5cb1f1b19d81bb0bce4d1
Victor Nazzaro <nazzav923@gmail.com>
2020-02-22 12:37:24 -0500

    changed creation to OOP

--- backend/template.py
+++ backend/template.py
@@ -1,9 +1,31 @@
+from database import Database
 class Template(object):
 
-    file_name = ""
-    tags = []
-    last_update = ""
-
-    def __init__(self, name, tags):
-        self.file_name = name
+    def __init__(self, name, filename, tags):
+        self.name = name
+        self.filename = filename
         self.tags = tags
+        self.description = "Just another template."
+        self.is_activated = True
+        self.is_deleted = True
+        self.origin_id = None
+        self.versions = None
+        self.db = Database()
+
+    def set_description(self, new_description):
+        self.description = new_description
+
+    def save_to_db(self):
+        self.db.create_template(self.name, self.filename, self.tags, self.description)
+
+    def create_version(self):
+        pass
+
+    def get_version(self):
+        pass
+
+    def delete_version(self):
+        pass
+    
+    def activate_version(self):
+        pass
