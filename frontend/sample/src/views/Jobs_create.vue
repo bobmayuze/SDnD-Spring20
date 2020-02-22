f67e9421dccfb90079d58eedee68935bfe0da822
Ming <lumingabby@outlook.com>
2020-02-22 13:04:58 -0500

    added select templates & checklist

--- frontend/sample/src/views/Jobs_create.vue
+++ frontend/sample/src/views/Jobs_create.vue
@@ -13,6 +13,18 @@
         v-bind="formItemLayout"
         label="Templates"
       >
+      <a-select
+          mode="multiple"
+          labelInValue
+          :value="value"
+          placeholder="Select Templates"
+          style="width: 100%"
+          :filterOption="false"
+          :notFoundContent="fetching ? undefined : null"
+        >
+          <a-spin v-if="fetching" slot="notFoundContent" size="small"/>
+          <a-select-option v-for="d in data" :key="d.value">{{d.text}}</a-select-option>
+        </a-select>
       </a-form-item>
 
 
@@ -24,6 +36,9 @@
         v-bind="formItemLayout"
         label="China-North"
       >
+      <a-checkbox-group :options="plainOptions" v-model="checkedList" @change="onChange" />
+      <br/>
+      <a-checkbox-group :options="plainOptions_2" v-model="checkedList" @change="onChange" />
       </a-form-item>
 
       <!-- Submit Button -->
@@ -39,6 +54,10 @@
 </style>
 
 <script>
+const plainOptions = ['Hangzhou', 'Beijing', 'Shanghai']
+const plainOptions_2 = ['Hongkong', 'German', 'Sydney']
+const defaultCheckedList = ['Hangzhou', 'Beijing']
+
 export default {
   data() {
     this.formItemLayout = {
@@ -46,13 +65,21 @@ export default {
       wrapperCol: { span: 10 },
     }
     return {
+      checkedList: defaultCheckedList,
+      plainOptions,
+      plainOptions_2,
       data: [],
       value: [],
       fetching: false,
     }
   },
   methods: {
-    
+    onChange (checkedList) {
+      console.log('onChange');
+      
+      this.indeterminate = !!checkedList.length && (checkedList.length < plainOptions.length)
+      this.checkAll = checkedList.length === plainOptions.concat(plainOptions_2).length
+    },
   }
 }
 </script>
