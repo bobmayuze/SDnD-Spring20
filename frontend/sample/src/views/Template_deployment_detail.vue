4661b9df89dc96b1b7763e01335ed839e0b7cbe1
Jade <wangyujue19960222@gmail.com>
2020-02-24 18:24:39 -0500

    deployment details demo

--- frontend/sample/src/views/Template_deployment_detail.vue
+++ frontend/sample/src/views/Template_deployment_detail.vue
@@ -1,5 +1,88 @@
 <template>
 
-<h1>This is Template Deployment Detail Page</h1>
+    <a-form id="components-form-demo-validate-other">
+        <a-row>
+            <a-col :span="6"></a-col>
+            <h1>Template Deployment Detail</h1>
+        </a-row>
+
+        <!-- {{template_info}} -->
+
+        <br/>
+        <a-form-item
+            v-bind="formItemLayout"
+            label="Target Region"
+        >
+            <h2>{{template_info.target_region}}</h2>
+        </a-form-item>
+
+        <a-form-item
+            v-bind="formItemLayout"
+            label="Created At"
+        >
+            <h2>{{template_info.create_time}}</h2>
+        </a-form-item>
+
+        <a-form-item
+            v-bind="formItemLayout"
+            label="Status"
+        >
+            <h2>{{template_info.status}}</h2>
+        </a-form-item>         
+ 
+        <a-row>
+            <a-col :span="6"/>
+
+            <!-- <a-col :span="8">              
+            </a-col> -->
+
+            <a-col :span="6">
+                <a-button 
+                    type="primary" 
+                    html-type="button"
+                    v-on:click="$router.go(-1)"
+                >
+                    Go Back
+                </a-button>
+            </a-col>            
+            
+        </a-row>
+        <!-- {{template_info}} -->
+    </a-form>
+    
 
 </template>
+
+<script> 
+import axios from 'axios';
+export default {
+    mounted() {
+        this.fetch(this.$route.query.job_id, this);
+    },
+    data() {
+        return {
+            template_info : 'SOME INFO',
+            formItemLayout: {
+                labelCol: { span: 6 },
+                wrapperCol: { span: 14 },
+            },            
+        }
+    },
+    methods : {
+        fetch(job_id, element){   
+            
+        },
+        buttonClieked(){
+            console.log('Clicked');
+            
+        }
+    }
+}
+</script> 
+
+<style>
+#components-form-demo-validate-other .dropbox {
+  height: 180px;
+  line-height: 1.5;
+}
+</style>
