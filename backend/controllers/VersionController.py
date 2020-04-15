497fa64e8249a3e6fe3c3620253db204aa1ad798
Victor Nazzaro <nazzav923@gmail.com>
2020-04-15 18:20:53 -0400

    fix activate version

--- backend/controllers/VersionController.py
+++ backend/controllers/VersionController.py
@@ -28,12 +28,12 @@ def create_version():
 
 @version_controller.route("/versions", methods=["PUT"])
 def activate_version():
-    # TODO: Get Version Object
     req_json = request.get_json()
     origin_id = req_json['origin_id']
     version_id = req_json['version_id'] 
     template = Template.getTemplate(origin_id)
     template.activate_version(version_id)
+    db.update_template(template)
     # resp = Response(response=db.activate_version(origin_id, version_id), status=200, mimetype="version_controllerlication/json")
     return {'msg': 'version activated succesfully'}
 
